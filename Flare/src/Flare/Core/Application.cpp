38a994da30a0ab8f3763ddebfdef6b854724033f
jumailj <jumailj.work@gmail.com>
2024-02-07 17:36:27 +0530

    code cleanup

--- Flare/src/Flare/Core/Application.cpp
+++ Flare/src/Flare/Core/Application.cpp
@@ -11,9 +11,6 @@ namespace Flare {
     // static pointer to a instance, singleton- behavior
     Application* Application::s_Instance = nullptr;
 
-
-
-
     Application::Application(const std::string&name)
    {
        // simply points to the current Application Object.
@@ -35,19 +32,46 @@ namespace Flare {
 			 0.0f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
 		};
 
-        m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices) ));
+        std::shared_ptr<VertexBuffer> vetexBuffer;
+        vetexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices) ));
         
         BufferLayout layout= {
         {ShaderDataType::Float3, "a_Position"},
         {ShaderDataType::Float4, "a_Color"},
         };
 
-        m_VertexBuffer->SetLayout(layout);  
-        m_VertexArray->AddVertexBuffer(m_VertexBuffer);
+        vetexBuffer->SetLayout(layout);  
+        m_VertexArray->AddVertexBuffer(vetexBuffer);
+
+        std::shared_ptr<IndexBuffer> indexBuffer;
 
 		unsigned int indices[3] = {0,1,2};
-        m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices)/sizeof(uint32_t)));
-		m_VertexArray->SetIndexBuffer(m_IndexBuffer);
+        indexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices)/sizeof(uint32_t)));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+
+        m_SquareVA.reset(VertexArray::Create());
+		float squareVertices[3 * 4] = {
+			-0.75f, -0.75f, 0.0f,
+			 0.75f, -0.75f, 0.0f,
+			 0.75f,  0.75f, 0.0f,
+			-0.75f,  0.75f, 0.0f
+		};
+
+        std::shared_ptr<VertexBuffer> squareVB;
+		squareVB.reset(VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+		squareVB->SetLayout({
+			{ ShaderDataType::Float3, "a_Position" }
+			});
+		m_SquareVA->AddVertexBuffer(squareVB);
+
+		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+		std::shared_ptr<IndexBuffer> squareIB;
+		squareIB.reset(IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+		m_SquareVA->SetIndexBuffer(squareIB);
+
+
+
 
 
         std::string vertexSrc = R"(
@@ -84,6 +108,36 @@ namespace Flare {
         )";
 
         m_Shader.reset( new Shader(vertexSrc,fragmentSrc));
+
+
+        std::string blueShaderVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			out vec3 v_Position;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string blueShaderFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+
+		m_BlueShader.reset(new Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 
     }
     
@@ -141,9 +195,14 @@ namespace Flare {
 			glClear(GL_COLOR_BUFFER_BIT);
 
 
+			m_BlueShader->Bind();
+			m_SquareVA->Bind();
+			glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+
+
             m_Shader->Bind();
             m_VertexArray->Bind();
-			glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(),GL_UNSIGNED_INT , nullptr);
+			glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(),GL_UNSIGNED_INT , nullptr);
 
             for (Layer* layer: m_LayerStack) {
                layer->OnUpdate();
