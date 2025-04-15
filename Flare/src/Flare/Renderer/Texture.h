f94f80414be4a1faa6c2b1c82db76dbd8dd82428
jumailj <jumailj.work@proton.me>
2025-04-15 15:23:57 +0530

    Texture serialize and deserialize added

--- Flare/src/Flare/Renderer/Texture.h
+++ Flare/src/Flare/Renderer/Texture.h
@@ -14,6 +14,11 @@ namespace Flare{
         virtual uint32_t GetHeight() const = 0;
         virtual uint32_t GetRendererID() const = 0;
 
+        // for storing texture path for serialization and deserialization.
+        virtual const std::string& GetPath() const = 0;
+
+
+
         virtual void SetData(void*data, uint32_t size ) = 0;
 
         virtual void Bind(uint32_t slot = 0) const = 0;
