dc348e4ee8ca90a3108075ba6c6a610794331d4d
jumailj <jumailj.work@proton.me>
2025-04-17 22:34:22 +0530

    removed player.cs file

--- Flare-ScriptCore/Source/Player.cs
+++ Flare-ScriptCore/Source/Player.cs
@@ -1,22 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace Sandbox {
+// namespace Sandbox {
 
-    internal class Player : Entity{
+//     internal class Player : Entity{
 
-        void OnCreate(){
-            Console.WriteLine("Player.Created!");
-        }
+//         void OnCreate(){
+//             Console.WriteLine("Player.Created!");
+//         }
 
-        void OnUpdate(float ts){
-            Console.WriteLine($"Player.Update: {ts}");
+//         void OnUpdate(float ts){
+//             Console.WriteLine($"Player.Update: {ts}");
 
-        }
+//         }
 
-    }
+//     }
 
-}
+// }
